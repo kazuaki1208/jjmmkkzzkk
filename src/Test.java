ac3bf88dd030e1603e5bbc1e03ede88143df57a4
jmkzk <jmkzk@jmkzk-PC>
2017-11-18 15:30:11 +0900

    20171118

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,2,3);
+		list.forEach(t -> System.out.println(t));
+	}
+
+}
